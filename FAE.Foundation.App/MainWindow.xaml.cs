73b407e5385ab024e26351edfe0e664193fd4c37
ngocminh76 <83106142+ngocminh76@users.noreply.github.com>
2026-08-10 21:52:01 +0700

    Cập nhập UI

--- FAE.Foundation.App/MainWindow.xaml.cs
+++ FAE.Foundation.App/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
 using System.Windows;
-using FAE.Foundation.App.ViewModels;
+using FAE.Foundation.App.Functions.MongBan.Views;
 
 namespace FAE.Foundation.App
 {
@@ -8,7 +8,25 @@ namespace FAE.Foundation.App
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainViewModel();
+        }
+
+        private void BtnMongTru_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: mở MongTruView khi có
+            MessageBox.Show("Chức năng Móng Trụ đang phát triển.", "Thông báo");
+        }
+
+        private void BtnMongBan_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new MongBanView();
+            win.Owner = this;
+            win.Show();
+        }
+
+        private void BtnMongCoc_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: mở MongCocView khi có
+            MessageBox.Show("Chức năng Móng Cọc đang phát triển.", "Thông báo");
         }
     }
 }
